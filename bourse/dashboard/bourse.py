22961fac8189007c6d6cc22ecf2689f00ed81de7
Léo DEVIN <leo.devin@epita.fr>
2024-03-24 21:56:21 +0100

    date + selectionner plusieurs companies par leur nom + try chandelier

--- bourse/dashboard/bourse.py
+++ bourse/dashboard/bourse.py
@@ -4,9 +4,10 @@ import dash.dependencies as ddep
 import pandas as pd
 import sqlalchemy
 import plotly.express as px
+import plotly.graph_objects as go
 import datetime as dt
 
-import logging
+import logging  
 
 LOG = logging.getLogger(__name__)
 
@@ -21,7 +22,7 @@ server = app.server
 
 # Search bar with smart search (dropdown)
 companies = pd.read_sql_query("SELECT * FROM companies", engine)
-company_options = [{'label': row['company_name'], 'value': row['cid']} for index, row in companies.iterrows()]
+company_options = [{'label': row['name'], 'value': row['id']} for index, row in companies.iterrows()]
 
 app.layout = html.Div([
                 dcc.Textarea(
@@ -60,7 +61,6 @@ app.layout = html.Div([
                     start_date=dt.datetime(2019, 1, 1),
                     end_date=dt.datetime.now()
                 ),
-                dcc.Input(id='company-id-input', type='number', placeholder='Enter company ID...'),
                 dcc.Graph(id='stock-graph')
              ])
 
@@ -81,28 +81,55 @@ def run_query(n_clicks, query):
 
 @app.callback(
     ddep.Output('stock-graph', 'figure'),
-    [ddep.Input('company-id-input', 'value')]
+    [ddep.Input('company-id-input', 'value'),
+     ddep.Input('date-picker-range', 'start_date'),
+     ddep.Input('date-picker-range', 'end_date'),
+     ddep.Input('graph-type', 'value')]
 )
-def update_graph(company_id):
-    if company_id is None:
+def update_graph(companies, start_date, end_date, graph_type='line'):
+    if companies is None:
         return {}
-    # Requête SQL pour récupérer les données de l'entreprise spécifique
-    query = f"""
-        SELECT date, value
-        FROM stocks
-        WHERE cid = {company_id}
-        ORDER BY date
-    """
 
+    start_date = start_date.split('T')[0]
+    end_date = end_date.split('T')[0]
 
-    # Charger les données directement en DataFrame Pandas en utilisant la requête SQL
-    df = pd.read_sql_query(query, engine, index_col='date', parse_dates=['date'])
-    
-    # Créer un graphe à partir des données en utilisant Plotly Express
-    fig = px.line(df, x=df.index, y='value', labels={'x': 'Date', 'y': 'Prix de l\'action'}, 
-                  title='Évolution du prix de l\'action pour une entreprise spécifique')
-    
-    return fig
+    start_date = dt.datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = dt.datetime.strptime(end_date, '%Y-%m-%d')
+
+    # Format the start_date and end_date to be compatible with SQL
+    start_date = start_date.strftime('%Y-%m-%d')
+    end_date = end_date.strftime('%Y-%m-%d')
+
+    # SQL query to retrieve data for the specific company within the selected date range
+    data = []
+
+    for company_id in companies:
+        # SQL query to retrieve data for the specific company within the selected date range
+        query = f"""
+            SELECT date, value
+            FROM stocks
+            WHERE cid = {company_id} AND date >= '{start_date}' AND date <= '{end_date}'
+            ORDER BY date
+        """
+
+        # Load data directly into a Pandas DataFrame using the SQL query
+        df = pd.read_sql_query(query, engine, index_col='date', parse_dates=['date'])
+
+        if graph_type == 'candlestick':
+            # Create a graph from the data using Plotly Graph Objects
+            fig = go.Figure(data=[go.Candlestick(x=df.index,
+                                                 open=df['value'],
+                                                 high=df['value'],
+                                                 low=df['value'],
+                                                 close=df['value'])])
+        else:
+            # Create a graph from the data using Plotly Express
+            fig = px.line(df, x=df.index, y='value', labels={'x': 'Date', 'y': 'Stock Price'}, 
+                          title=f'Stock Price Evolution for Company {company_id}')
+
+        data.append(fig)
+
+    return {'data': data}
 
 if __name__ == '__main__':
     app.run(debug=True)
