0f3da8fde64d615d03d1b2448c4193336e3833fc
Léo DEVIN <leo.devin@epita.fr>
2024-03-25 15:08:31 +0100

    nouvelle strat

--- bourse/analyzer/analyzer.py
+++ bourse/analyzer/analyzer.py
@@ -10,12 +10,12 @@ import timescaledb_model as tsdb
 db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'db', 'monmdp')        # inside docker
 #db = tsdb.TimescaleStockMarketModel('bourse', 'ricou', 'localhost', 'monmdp') # outside docker
 
-def store_file(name, website):
-    if db.is_file_done(name):
+def store_file(filename, website):
+    if db.is_file_done(filename):
         return
     if website.lower() == "boursorama":
         market_id = "0"
-        market_alias = name.split()[0]
+        market_alias = filename.split()[0]
         if market_alias == "peapme":
             market_id = db.raw_query("SELECT id FROM markets WHERE alias = %s", ("euronx",))[0][0]
             pea = True
@@ -24,10 +24,10 @@ def store_file(name, website):
             pea = False
 
         try:
-            df = pd.read_pickle("data/boursorama/" + name)  # is this dir ok for you ?
+            df = pd.read_pickle("data/boursorama/" + filename)  # is this dir ok for you ?
         except:
-            year = name.split()[1].split("-")[0]
-            df = pd.read_pickle("data/boursorama/" + year + "/" + name)
+            year = filename.split()[1].split("-")[0]
+            df = pd.read_pickle("data/boursorama/" + year + "/" + filename)
 
 
         df['last'] = df['last'].str.replace('(c)', '')
@@ -35,7 +35,7 @@ def store_file(name, website):
         df['last'] = df['last'].str.replace(' ', '').astype(float)
 
         #DATE
-        date_string = name.split(' ')[1] + " " +  name.split(' ')[2]
+        date_string = filename.split(' ')[1] + " " +  filename.split(' ')[2]
         date_string = date_string.replace('.bz2', '')
 
         companies = df['name'].unique()
@@ -44,51 +44,68 @@ def store_file(name, website):
         columns = ['name', 'mid', 'symbol', 'symbol_nf', 'isin', 'reuters', 'boursorama', 'pea', 'sector']
         new_companies = []
 
-        for name in companies:
-            company_id = db.search_company_id(name)
+        for company_name in companies:
+            company_id = db.search_company_id(company_name)
             if company_id == 0:
-                new_companies.append({'name': name, 'mid': int(market_id), 'pea': pea})
+                new_companies.append({'name': company_name, 'mid': int(market_id), 'pea': pea})
 
         if new_companies != []:
             new_companies = pd.DataFrame(new_companies, columns=columns)
-            db.df_write(new_companies, 'companies', index=False, if_exists='append')
+            #db.df_write(new_companies, 'companies', index=False, if_exists='append')
             print("New companies added to the database.")
         else:
             print("No new companies to add to the database.")
 
 
         #STOCKS
+        columns = ['date', 'cid', 'value', 'volume']
         stocks_df = pd.DataFrame({'date': [date_string] * len(df),
                                     'cid': df['name'].apply(lambda x: db.search_company_id(x)),
                                     'value': df['last'],
-                                    'volume': df['volume']})
-        db.df_write(stocks_df, 'stocks', index=False, if_exists='append')
+                                    'volume': df['volume']},
+                                    columns=columns)
+        #db.df_write(stocks_df, 'stocks', index=False, if_exists='append')
 
 
         #DAYSTOCKS
+        """columns = ['date', 'cid', 'close', 'volume', 'open', 'high', 'low']
         daystocks_df = pd.DataFrame({'date': [date_string] * len(df),
                                      'cid': df['name'].apply(lambda x: db.search_company_id(x)),
-                                     'close': df['last'],
-                                     'volume': df['volume']
-        })
+                                     'volume': df['volume']},
+                                     columns=columns)
 
         daystocks_df['open'] = stocks_df.groupby('cid')['value'].transform('first')
+        daystocks_df['close'] = stocks_df.groupby('cid')['value'].transform('last')
         daystocks_df['high'] = stocks_df.groupby('cid')['value'].transform('max')
         daystocks_df['low'] = stocks_df.groupby('cid')['value'].transform('min')
 
-        db.df_write(daystocks_df, 'daystocks', index=False, if_exists='append')
+
+        db.df_write(daystocks_df, 'daystocks', index=False, if_exists='append')"""
                                      
-        db.df_write(pd.DataFrame({'name': [name]}), 'file_done', index=False, if_exists='append')
+        db.df_write(pd.DataFrame({'name': [filename]}), 'file_done', index=False, if_exists='append')
         print("File stored in the database.")
 
         
 
+def store_files(market, year):
+    files = glob.glob("data/boursorama/" + year + "/" + market+ "*")
+    df = pd.concat([pd.read_pickle(file) for file in files], keys=[pd.to_datetime(file.split(' ')[-2] + ' ' + file.split(' ')[-1].split('.bz2')[0], format='%Y-%m-%d %H:%M:%S.%f') for file in files], names=['date'])
+
+    df['last'] = df['last'].str.replace('(c)', '')
+    df['last'] = df['last'].str.replace('(s)', '')
+    df['last'] = df['last'].str.replace(' ', '').astype(float)
+
+    df.reset_index(level=0, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    #get list of unique companies, 1 company can have multiple symbols
 
 
+    
 
 if __name__ == '__main__':
     TEST = True
-    store_file("amsterdam 2021-12-31 14:52:01.606661.bz2", "boursorama")
+    store_files("compA", "2020")
     if TEST:
         """path = "data/boursorama/2020"
 
@@ -100,10 +117,9 @@ if __name__ == '__main__':
     else:
         path = "data/boursorama/"
 
-        for years in os.walk(path):
-            print(years)
-            for file in years:
-                if not db.is_file_done(file):
+        for dirs in os.walk(path):
+            for files in dirs:
+                for file in files:
                     print("Storing file " + file)
                     store_file(file, "boursorama")
 
